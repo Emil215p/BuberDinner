0de45b9d015831cec5d6b2b8d947d5feeac45043
Emil215p <juelemil@gmail.com>
2024-02-07 14:19:26 +0100

    More changes

--- BuberDinner.Application/obj/Debug/net8.0/BuberDinner.Application.AssemblyInfo.cs
+++ BuberDinner.Application/obj/Debug/net8.0/BuberDinner.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberDinner.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcac4de98c4e273502abac5bc25f70355fe0841a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59171d5252195d4b5daa55d0c52e05c10c384192")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberDinner.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberDinner.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
